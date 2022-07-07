cf3c2f1414aeda3f0d4659b5a94e99fdeee1aa46
Sergio Sepulveda <contacto@penti2s.com>
2022-07-07 12:53:33 -0400

    Creacion de login dinamico

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -7,40 +7,48 @@
 
             <v-spacer></v-spacer>
 
+
+            <v-toolbar-title>{{ email }}</v-toolbar-title>
+
             <v-btn icon>
-                <v-icon>mdi-heart</v-icon>
+                <v-icon
+                    v-if="login"
+                    @click="logOut"
+                >mdi-logout-variant</v-icon>
             </v-btn>
 
             <v-btn icon>
                 <v-icon>mdi-magnify</v-icon>
             </v-btn>
 
-            <v-menu left bottom>
-                <template v-slot:activator="{ on, attrs }">
-                    <v-btn icon v-bind="attrs" v-on="on">
-                        <v-icon>mdi-dots-vertical</v-icon>
-                    </v-btn>
-                </template>
-
-                <v-list>
-                    <v-list-item v-for="n in 5" :key="n" @click="() => { }">
-                        <v-list-item-title>Option {{ n }}</v-list-item-title>
-                    </v-list-item>
-                </v-list>
-            </v-menu>
         </v-app-bar>
     </div>
 </template>
 
 <script>
-import { getAuth } from "firebase/auth";
+import { getAuth, signOut } from "firebase/auth";
 export default {
     name: 'nav-bar',
     data() {
         return {
             login: false,
+            email: '',
         }
     },
+    methods: {
+        logOut(){
+
+
+            const auth = getAuth();
+            signOut(auth).then(() => {
+            // Sign-out successful.
+                this.$router.push('/login')
+            }).catch((error) => {
+            // An error happened.
+                console.log(error)
+            });
+        }
+    },  
     created() {
         const auth = getAuth();
         const user = auth.currentUser;
@@ -49,7 +57,10 @@ export default {
         // User is signed in, see docs for a list of available properties
         // https://firebase.google.com/docs/reference/js/firebase.User
         // ...
+            console.log("User is signed in");
+            console.log(user)
             this.login = true
+            this.email = user.email
         } else {
         // No user is signed in.
         }
